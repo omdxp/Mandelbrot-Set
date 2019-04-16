463fd146da2f442191eaf1de881bce237a5f366a
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-04-16 07:47:53 +0100

    Initial commit

--- Mandelbrot set/Mandelbrot set.cpp
+++ Mandelbrot set/Mandelbrot set.cpp
@@ -3,10 +3,43 @@
 
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <complex>
+
+using namespace std;
+int value(int x, int y);
+// Image dimension
+constexpr float width = 600;
+constexpr float height = 600;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	ofstream image("mandelbrot.ppm");
+	if (image.is_open()) {
+		image << "P3\n" << width << " " << height << " 255\n";
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int val = value(i, j);
+				image << val << ' ' << 0 << ' ' << 0 << "\n";
+			}
+		}
+		image.close();
+	}
+	else cout << "Could not open file";
+	return EXIT_SUCCESS;
+}
+
+int value(int x, int y) {
+	complex<float> point((float)x / width - 1.5, (float)y / height - 0.5);
+	// we divide by the image dimensions to get values smaller than 1 then apply a translation
+	complex<float> z(0, 0);
+	unsigned int nb_iter = 0;
+	while (abs(z) < 2 && nb_iter <= 42) { // Answer of everything
+		z = z * z + point;
+		nb_iter++;
+	}
+	if (nb_iter < 42) return (255 * nb_iter) / 42; // Some sort of gradient color
+	else return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
